c27fbdbc6be247eef0ad12434c1fad5695105a8c
Иван Едоков <ivanedokov@yandex.ru>
2023-07-22 16:12:23 +0400

    приложения: рецептов, ингридиентов, api, пользователей

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -40,6 +40,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'api.apps.ApiConfig',
+    'ingredients.apps.IngredientsConfig',
+    'recipes.apps.RecipesConfig',
+    'users.apps.UsersConfig',
 ]
 
 MIDDLEWARE = [
