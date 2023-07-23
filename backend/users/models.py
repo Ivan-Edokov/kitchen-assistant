b20b71b78365d368502e9b633c2b2851c97326f2
Иван Едоков <ivanedokov@yandex.ru>
2023-07-23 12:11:14 +0400

    Модель и админка User

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,18 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    email = models.EmailField(
+        'email адрес',
+        blank=True,
+        max_length=254,
+    )
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+        ordering = ['username']
+
+    def __str__(self):
+        return self.username
